c89ebd96908d72677dec62b058a56c469c0e22b1
Aram Gurdzhinyan <gourj777@yandex.ru>
2021-12-14 19:17:46 +0300

    Riddle solved

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,17 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
+// Напишите ответ здесь: 100 чисел.
+#include <iostream>
 
+using namespace std;
+
+int main(){
+
+	cout << "How many numbers from 1 to 1000 are containing at least one digit 3?"s << endl;
+	short numbers = 1000;
+	//в каждый десяток содержит как минимум 1 цифру 3.
+	short digit_3_count = numbers/10;
+	cout << "There are 100 numbers, from 1 to 1000, that are containing at least one digit 3"s << endl;
+
+	return 0;
+}
 // Закомитьте изменения и отправьте их в свой репозиторий.
